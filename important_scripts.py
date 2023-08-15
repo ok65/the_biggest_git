97638b8ca38ebabc69c9f1b30cacf60871b79304
Oliver <mail.ok65@googlemail.com>
2023-08-15 10:17:24 +0100

    first commit

---
+++ important_scripts.py
@@ -0,0 +1,13 @@
+
+
+CONSTANT = 5
+
+
+def go_bananas():
+    for x in range(1000):
+        print("banana")
+
+
+if __name__ == "__main__":
+
+    go_bananas()
---
+++ updater.py
@@ -0,0 +1,15 @@
+
+# Library imports
+from git import Repo
+import os
+
+
+def update():
+    repo = Repo(os.getcwd())
+
+    pass
+
+
+if __name__ == "__main__":
+    update()
+    pass
